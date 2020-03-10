166bb960e45319efcd242d804d811833fcd25ab1
sixthkrum <mailbox.vikramalagh@gmail.com>
2020-03-11 02:27:37 +0530

    seems to be working for all symbol sizes greater than or equal to 8 now

--- frequency-analyser.cpp
+++ frequency-analyser.cpp
@@ -31,10 +31,10 @@ unsigned int cstring_to_int ( char* s , bool check_only )
   return num;
 }
 
-inline unsigned int carry_over_bitmask ( unsigned int leftover_bits )
+inline unsigned int carry_over_bitmask ( int leftover_bits )
 {
   unsigned int temp = 0;
-  for ( int i = 0 ; i < 8 - leftover_bits ; i ++)
+  for ( int i = 0 ; i < leftover_bits ; i ++)
   {
     temp = pow ( 2 , i ) + temp;
   }
@@ -79,7 +79,7 @@ int main(int argc , char* argv[])
 
   }
 
-  std::cout << "starting\n";
+  //std::cout << "starting\n";
 
   unsigned int symbol_bits;
   symbol_bits = cstring_to_int ( argv[2] , 0 );
@@ -91,6 +91,7 @@ int main(int argc , char* argv[])
   unsigned int partial_symbol = 0;
   unsigned int symbol = 0;
   unsigned int carry_over_bits = 0;
+  bool early_eof = 0;
   bool mod8_flag = [ leftover_bits ]() -> bool {
     if( leftover_bits == 0)
     {
@@ -138,15 +139,42 @@ int main(int argc , char* argv[])
   {
     while ( file.peek() != EOF)
     {
-      partial_symbol = 0;
-      file.read( (char*)&partial_symbol , full_symbol_bytes );
-      symbol = partial_symbol;
-      symbol = partial_symbol << leftover_bits;
-      partial_symbol = 0;
-      file.read( (char*)&partial_symbol , 1 );
-      carry_over_bits = partial_symbol & carry_over_bitmask ( leftover_bits );
-      symbol = symbol + ( partial_symbol >> ( 8 - leftover_bits ) );
-      leftover_bits = symbol_bits - ( 8 - leftover_bits ) - ( 8 * full_symbol_bytes );
+      symbol = 0;
+      for ( int i = 0 ; i < full_symbol_bytes ; i ++ )
+      {
+        partial_symbol = 0;
+        file.read( (char*)&partial_symbol , 1 );
+        symbol = symbol +  ( partial_symbol << ( ( full_symbol_bytes - i - 1 ) * 8 ) );
+
+        if( file.peek() == EOF )
+        {
+          symbol = symbol >> ( ( full_symbol_bytes - i - 1 ) * 8 );
+          break;
+        }
+      }
+
+      symbol = ( carry_over_bits << ( 8 * full_symbol_bytes ) ) + symbol;
+
+      if ( leftover_bits > 0 && file.peek() != EOF )
+      {
+        partial_symbol = 0;
+        file.read( (char*)&partial_symbol , 1 );
+        symbol = ( symbol << leftover_bits ) + ( partial_symbol >> ( 8 - leftover_bits ) );
+        carry_over_bits = partial_symbol & carry_over_bitmask (  8 - leftover_bits );
+        leftover_bits = symbol_bits - ( 8 - leftover_bits ) - ( 8 * full_symbol_bytes );
+      }
+
+      else if ( leftover_bits <= 0 )
+      {
+        symbol =  ( symbol >> ( - leftover_bits ) );
+        carry_over_bits = partial_symbol & carry_over_bitmask (  - leftover_bits );
+        leftover_bits = symbol_bits + leftover_bits - ( 8 * full_symbol_bytes );
+      }
+
+      else
+      {
+        early_eof = 1;
+      }
 
       try
       {
@@ -158,34 +186,21 @@ int main(int argc , char* argv[])
         symbol_map.insert ( std::pair < unsigned int , unsigned int > ( symbol , 1 ) );
       }
 
-      if( file.eof() )
-      {
-        break;
-      }
+    }
+  }
 
-      partial_symbol = 0;
-      file.read( (char*)&partial_symbol , full_symbol_bytes - 1);
-      symbol = ( carry_over_bits << ( full_symbol_bytes * 8 + leftover_bits ) ) + ( partial_symbol << ( leftover_bits + 8 ) );
+  if ( ! early_eof )
+  {
+    symbol = carry_over_bits;
 
-      if ( leftover_bits < 0 )
-      {
-        partial_symbol = 0;
-        file.read( (char*)&partial_symbol , 1);
-        symbol = symbol + ( partial_symbol >> ( - leftover_bits ) );
-        carry_over_bits = partial_symbol & carry_over_bitmask ( - leftover_bits );
-        leftover_bits = symbol_bits + leftover_bits - full_symbol_bytes * 8;
-      }
+    try
+    {
+      symbol_map [ symbol ] ++;
+    }
 
-      try
-      {
-        symbol_map [ symbol ] ++;
-      }
-
-      catch ( std::out_of_range )
-      {
-        symbol_map.insert( std::pair < unsigned int , unsigned int > ( symbol , 1 ) );
-      }
-
+    catch ( std::out_of_range )
+    {
+      symbol_map.insert ( std::pair < unsigned int , unsigned int > ( symbol , 1 ) );
     }
   }
 
